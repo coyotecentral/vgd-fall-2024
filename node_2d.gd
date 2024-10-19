9d02dfcf166169683d0914a2a74b91b66a0d2e34
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2024-10-19 15:28:48 -0700

    checkpoint

--- node_2d.gd
+++ node_2d.gd
@@ -5,9 +5,12 @@ const tile_size = 64.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Timer.timeout.connect(tick)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func tick():
 	$Sprite2D.position += Vector2.RIGHT * tile_size
