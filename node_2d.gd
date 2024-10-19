1c8d32ffb706510cffd4efa618eaf698adf6b094
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2024-10-19 15:18:14 -0700

    Updates

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const tile_size = 64.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +10,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Sprite2D.position += 10.0 * Vector2(1.0, 1.0) * delta
+	$Sprite2D.position += Vector2.RIGHT * tile_size
