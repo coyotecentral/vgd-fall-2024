f14a229a540f20fd8f50614c68221d35c37f1bed
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2024-10-26 15:41:46 -0700

    Checkpoint

--- node_2d.gd
+++ node_2d.gd
@@ -10,7 +10,8 @@ var snake = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	snake.push_front($Sprite2D)
-	spawn_segment()
+	spawn_segment(Vector2())
+	spawn_segment(Vector2())
 	$Timer.timeout.connect(tick)
 	$Timer.start()
 
@@ -24,12 +25,18 @@ func _process(delta):
 
 
 func tick():
-	$Sprite2D.position += direction * tile_size
+	var next_pos = direction * tile_size
+	# snake[0] gets the first segment of our snake
+	var head_pos = snake[0].position
+	spawn_segment(head_pos + next_pos)
+	snake.pop_back().queue_free()
 
 # This function is going to create a new
 # snake segment and add it to the world. We're
 # also going to add it to our snake array.
-func spawn_segment():
+func spawn_segment(position):
 	# Create an "instance" of the scene
 	var new_segment = sprite.instantiate()
 	add_child(new_segment)
+	snake.push_front(new_segment)
+	new_segment.position = position
